adc88985686f46405420dde6c432b10fdd81b3c4
Jason <jasonbrandon9406@gmail.com>
2017-04-21 12:39:33 +0100

    commit of all file changes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  
-})
+  function Box(make, model, topspeed){
+    this.make = make;
+    this.model = model;
+    this.topspeed = topspeed;
+    this.carMessage = function(){
+
+
+    }
+
+  }
+});
